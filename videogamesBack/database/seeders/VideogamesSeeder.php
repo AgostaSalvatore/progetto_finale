690104e1c348c5da30dfb7bd20cba0ad3c992f8a
AgostaSalvatore <agosta.boolean@gmail.com>
2025-09-23 12:18:27 +0200

    first commit

---
+++ videogamesBack/database/seeders/VideogamesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Videogame;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VideogamesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newVideogame = new Videogame();
+
+            $newVideogame->title        = $faker->sentence(3);
+            $newVideogame->platform     = $faker->word();
+            $newVideogame->release_date = $faker->date();
+            $newVideogame->description  = $faker->text();
+            $newVideogame->price        = $faker->randomFloat(2, 0, 100);
+            $newVideogame->cover_image  = $faker->image();
+
+            $newVideogame->save();
+        }
+    }
+}
